29aa72b436c39c10e7e4cd9bb124e3bb19aed52a
abey1 <bruckabey@gmail.com>
2022-05-02 21:31:57 +0300

    skills finished

--- mywork/app.js
+++ mywork/app.js
@@ -1,7 +1,17 @@
 document.querySelector(".fa-bars").addEventListener("click", () => {
-  var nav = (document.getElementById("nav").style.left = "0");
+  var nav = document.getElementById("nav");
+  nav.style.left = "0";
+  var menuHolder = document.getElementById("menu-holder");
+  menuHolder.style.boxShadow = "0 0.5em 3em #000";
 });
 
 document.querySelector(".fa-xmark").addEventListener("click", () => {
-  document.getElementById("nav").style.left = "-100%";
+  var nav = document.getElementById("nav");
+  nav.style.left = "-200%";
+  var menuHolder = document.getElementById("menu-holder");
+  menuHolder.style.boxShadow = "0 0 0 #000;";
+});
+
+window.addEventListener("resize", () => {
+  document.getElementById("window-width").textContent = window.innerWidth;
 });
